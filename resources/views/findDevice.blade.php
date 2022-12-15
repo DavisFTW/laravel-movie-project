67051d9c45782f8b574836eca24f43a7cd33de5d
Davis <davis@davis.com>
2022-12-15 16:16:56 +0200

    small changes

--- resources/views/findDevice.blade.php
+++ resources/views/findDevice.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.master')
-
 @section('content')
 <div class="card m-2">
     <div class="card-header">
       ---- Movie name ---- 
     </div>
-    <div class="card-body">
+  <div class="row">
+    <div class="col">
+      img
+    </div>
+    <div class="col">
+      text
+    </div>
   </div>
 </div>
-
 @endsection
